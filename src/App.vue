d77e1224728ce9b775371420d615506f2ff5d3e9
Njinga Nkosi <47808032+Njinga@users.noreply.github.com>
2020-05-30 02:24:51 +0200

    more maths

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,39 @@
 <template>
-<div id="app">
-    <h4 class="bg-primary text-white text-center p-2">QUADRATIC EQUATIONS</h4>
-</div>
+    <div id="app" class="shadow-lg p-3 mb-5 bg-light rounded">
+        <h1 class="bg-secondary text-white text-center p-2"><strong>INTRODUCTION TO AGELBRA</strong></h1>
+        <p class="text-primary">SOLVING QUADRATIC EQUATIONS</p>
+        <p class="text-danger">Completing the square</p>
+        <p>We can complete the square to solve a Quadratic Equation (find where it is equal to zero).</p>
+
+        <p>But a general Quadratic Equation can have a coefficient of <strong>a</strong> in front of x<sup>2</sup>:<p>
+
+        <p> <strong>ax<sup>2</sup>+bx+c=0</strong></p>
+
+        <p>But that is easy to deal with ... just divide the whole equation by "a" first, then carry on:</p>
+
+        <p><strong>x<sup>2</sup> + (b/a)x + c/a = 0</strong><p>
+
+        <p class="text-primary">Steps</p>
+
+        <p>Now we can solve a quadratic equaton in 5 steps:</p>
+
+        <p>
+            <strong>. Step 1</strong> Divide all terms by <strong>a</strong>(the coffient of x<sup>2</sup>)<br />
+            <strong>. Step 2</strong> Move the number term (<strong>c/a</strong>) to the right side of the equation.<br />
+            <strong>. Step 3</strong> Complete the square on the left side of the equation and
+            balance this by adding the same value to the right side of the
+            equation.
+        </p>
+
+        <p>We now have something that looks like (x + p)<sup>2</sup> = q, which can be solved rather easily:<p>
+
+        <p><strong>. Step 4</strong>Take the square root on both sides of the equation.</p>
+        <p>
+            <strong>. Step 5</strong>Subtract the number that remains on the left side of the
+            equation to find <strong>x</strong>.
+        </p>
+
+    </div>
 </template>
 
 <script>
